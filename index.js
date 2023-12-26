bb6d20b77a2a0d5ee5d378229761c6d61f452fc0
Roman Le Négrate <roman.lenegrate@gmail.com>
2023-12-26 06:55:33 +0100

    Change indentation from 4 to 2 spaces

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Express = require('express');
-const cors    = require('cors');
-const net     = require('net');
-const Web3    = require('web3');
+const cors  = require('cors');
+const net   = require('net');
+const Web3  = require('web3');
 const dotenv  = require('dotenv');
 
 dotenv.config({}); 
@@ -22,61 +22,61 @@ const provider = () => {
 const web3 = new Web3(provider());
 const app =
   Express().
-    use(Express.json({ limit: '50mb' })).
-    use(cors());
+  use(Express.json({ limit: '50mb' })).
+  use(cors());
 
 app.post('/nonces', async (request, response) => {
-    const accounts = request.body.accounts;
-    const tasks = [];
-    accounts.forEach((account) => {
-        tasks.push(
-            new Promise(async (resolve, reject) => {
-                try {
-                    const count = await web3.eth.getTransactionCount(account); 
-                    resolve({ account, count }); 
-                } catch (error) {
-                    resolve({ account, count: 0 });
-                }
-            })
-        )
-    }); 
-    response.send(await Promise.all(tasks)); 
+  const accounts = request.body.accounts;
+  const tasks = [];
+  accounts.forEach((account) => {
+    tasks.push(
+      new Promise(async (resolve, reject) => {
+        try {
+          const count = await web3.eth.getTransactionCount(account); 
+          resolve({ account, count }); 
+        } catch (error) {
+          resolve({ account, count: 0 });
+        }
+      })
+    )
+  }); 
+  response.send(await Promise.all(tasks)); 
 });
 
 app.post('/contract-codes', async (request, response) => {
-    const contracts = request.body.contracts;
-    const tasks = [];
-    contracts.forEach((contract) => {
-        tasks.push(
-            new Promise(async (resolve, reject) => {
-                try {
-                    const code = await web3.eth.getCode(contract); 
-                    resolve({ contract, code }); 
-                } catch (error) {
-                    resolve({ contract, code: "0x" });
-                }
-            })
-        )
-    }); 
-    response.send(await Promise.all(tasks)); 
+  const contracts = request.body.contracts;
+  const tasks = [];
+  contracts.forEach((contract) => {
+    tasks.push(
+      new Promise(async (resolve, reject) => {
+        try {
+          const code = await web3.eth.getCode(contract); 
+          resolve({ contract, code }); 
+        } catch (error) {
+          resolve({ contract, code: "0x" });
+        }
+      })
+    )
+  }); 
+  response.send(await Promise.all(tasks)); 
 });
 
 app.post('/transaction-receipts', async (request, response) => {
-    const hashes = request.body.hashes;
-    const tasks = [];
-    hashes.forEach((hash) => {
-        tasks.push(
-            new Promise(async (resolve, reject) => {
-                try {
-                    const receipt = await web3.eth.getTransactionReceipt(hash); 
-                    resolve({ hash, receipt }); 
-                } catch (error) {
-                    resolve({ hash, receipt: null });
-                }
-            })
-        )
-    }); 
-    response.send(await Promise.all(tasks)); 
+  const hashes = request.body.hashes;
+  const tasks = [];
+  hashes.forEach((hash) => {
+    tasks.push(
+      new Promise(async (resolve, reject) => {
+        try {
+          const receipt = await web3.eth.getTransactionReceipt(hash); 
+          resolve({ hash, receipt }); 
+        } catch (error) {
+          resolve({ hash, receipt: null });
+        }
+      })
+    )
+  }); 
+  response.send(await Promise.all(tasks)); 
 });
 
 const port = process.env.PORT || 80;
